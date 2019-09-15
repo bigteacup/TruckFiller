9f27c4c38c572e75c34db305e5b3579facd5a281
bigteacup <coatim@gmail.com>
2019-09-15 21:31:49 +0200

    maj

--- app/src/main/java/com/example/truckfiller/Palette.java
+++ app/src/main/java/com/example/truckfiller/Palette.java
@@ -11,7 +11,94 @@ public class Palette {
     int positionY = 0;
     int positionXb = 0;
     int positionYb = 0;
+    int surface = 0;
 
+    public int getSurface() {
+        surface = largeur * hauteur;
+        return surface;
+    }
 
+    public void setSurface(int surface) {
+        this.surface = surface;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+
+    public int getLongueur() {
+        return longueur;
+    }
+
+    public void setLongueur(int longueur) {
+        this.longueur = longueur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+
+    public void setPoids(int poids) {
+        this.poids = poids;
+    }
+
+    public int getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(int orientation) {
+        this.orientation = orientation;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getPositionXb() {
+        return positionXb;
+    }
+
+    public void setPositionXb(int positionXb) {
+        this.positionXb = positionXb;
+    }
+
+    public int getPositionYb() {
+        return positionYb;
+    }
+
+    public void setPositionYb(int positionYb) {
+        this.positionYb = positionYb;
+    }
 }
--- app/src/main/java/com/example/truckfiller/Rang.java
+++ app/src/main/java/com/example/truckfiller/Rang.java
@@ -6,4 +6,49 @@ public class Rang {
     int idDuRang = 0;
     int NombreDEmplacements;
     ArrayList<Palette> listePaletteDuRang = new ArrayList<Palette>();
+    int surface = 0;
+
+    public int getSurface() {
+        surface = 0;
+        for(int i = 0; i < listePaletteDuRang.size(); i++){
+            surface = surface + (listePaletteDuRang.get(i).getSurface());
+        }
+            //surface = largeur * hauteur;
+        return surface;
+    }
+
+    public void setSurface(int surface) {
+        this.surface = surface;
+    }
+
+
+
+
+    public int getIdDuRang() {
+        return idDuRang;
+    }
+
+    public void setIdDuRang(int idDuRang) {
+        this.idDuRang = idDuRang;
+    }
+
+    public int getNombreDEmplacements() {
+        return NombreDEmplacements;
+    }
+
+    public void setNombreDEmplacements(int nombreDEmplacements) {
+        NombreDEmplacements = nombreDEmplacements;
+    }
+
+    public ArrayList<Palette> getListePaletteDuRang() {
+        return listePaletteDuRang;
+    }
+
+    public void setListePaletteDuRang(ArrayList<Palette> listePaletteDuRang) {
+        this.listePaletteDuRang = listePaletteDuRang;
+    }
+
+
+
+
 }
